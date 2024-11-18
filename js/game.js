5e67650ba04e60258d2fd31c1cef4e8eb1afd883
김민지 <128786831+cherish1a@users.noreply.github.com>
2024-11-18 22:56:03 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -205,7 +205,7 @@ function frame(){
         dino.y -= 7;
         jump_timer++;
 
-        if(jump_timer > 25){
+        if(jump_timer > 10){
             jump = false;
             jump_timer = 0;
         }
